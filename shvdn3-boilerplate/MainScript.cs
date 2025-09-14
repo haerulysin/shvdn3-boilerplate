88e8fd2cb3256947e8c816b7a0cc5b92abd2839b
krulx00 <aioresult@gmail.com>
2025-09-14 10:25:18 +0700

    first commit

---
+++ shvdn3-boilerplate/MainScript.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows.Forms;
+using GTA;
+using GTA.UI;
+
+namespace MyMod
+{
+    public class MainScript:Script
+    {
+        public MainScript()
+        {
+            Tick += OnTick;
+            KeyUp += OnKeyUp;
+            KeyDown += OnKeyDown;
+
+            Notification.Show("~g~MainScript Activated\n~w~Press F5 to Try");
+        }
+
+        private void OnTick(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OnKeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void OnKeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.F5)
+            {
+                Notification.Show("F5 Pressed!");
+            }
+        }
+    }
+}
